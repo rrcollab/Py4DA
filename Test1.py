f27a5f5d13018abd740400a410ca63ab514caf1d
rrcollab <richcollaborates@gmail.com>
2024-06-25 21:46:53 +0530

    git_integ_test1

---
+++ Test1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 21:29:41 2024
+
+@author: richc
+"""
+print("Hello")
---
+++ Test2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 21:45:18 2024
+
+@author: richc
+"""
+print("Welcom")
